a5ce3aef80f97a42bb7403cd756e048dfd2aba01
Eric <eric@nanopay.net>
2020-05-07 00:06:56 -0400

    Improve logger.dump for assoc type

--- bootstrapping/language/basefunctions/logger.js
+++ bootstrapping/language/basefunctions/logger.js
@@ -3,6 +3,15 @@ var dres = util.dres;
 
 var localUtil = require('./util');
 
+var makeDump = val => {
+  if ( val.type === 'assoc' ) {
+    return val.internal.map(entry =>
+      [entry.key,entry.value]
+    )
+  }
+  return val;
+};
+
 var lib = {};
 lib.logger = {
   'info': localUtil.newFunc(args => {
@@ -12,7 +21,7 @@ lib.logger = {
   }, null),
   'dump': (args, ctx) => {
     console.log('\x1B[37;1m[dump]\x1B[0m',
-      ...args.map(a => a));
+      ...args.map(a => makeDump(a)));
     return dres.resOK(null);
   },
   'notice': localUtil.newFunc(args => {
